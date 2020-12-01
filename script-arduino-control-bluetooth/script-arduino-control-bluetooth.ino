047ea9356ec7da3f4355b31d0bfb453e0b587df1
Romario <Romario@DESKTOP-UQ3S26U.local>
2020-12-01 14:47:32 -0500

    Update script-arduino-control-automatico.ino, script-arduino-control-bluetooth.ino, and 2 more files...

---
+++ script-arduino-control-bluetooth/script-arduino-control-bluetooth.ino
@@ -0,0 +1,63 @@
+
+#include <Servo.h>                
+Servo servo;                      // Crea el objeto servo con las caracteristicas de Servo
+
+int izqA = 5; 
+int izqB = 6; 
+int derA = 9; 
+int derB = 10; 
+int vel = 255;            // Velocidad de los motores (0-255)
+int estado = 'c';         // inicia detenido
+
+
+
+void setup()  { 
+  Serial.begin(9600);    // inicia el puerto serial para comunicacion con el Bluetooth
+  pinMode(derA, OUTPUT);
+  pinMode(derB, OUTPUT);
+  pinMode(izqA, OUTPUT);
+  pinMode(izqB, OUTPUT);
+  
+
+  servo.attach(11,660,1400);    // Asocia el servo al pin 11, define el min y max del ancho del pulso  
+                                 // eso depende del fabricante del servo
+} 
+
+void loop()  { 
+
+  if(Serial.available()>0){        // lee el bluetooth y almacena en estado
+    estado = Serial.read();
+  }
+  if(estado=='a'){           // Boton desplazar al Frente
+    analogWrite(derB, 0);     
+    analogWrite(izqB, 0); 
+    analogWrite(derA, vel);  
+    analogWrite(izqA, vel);       
+  }
+  if(estado=='b'){          // Boton IZQ 
+    analogWrite(derB, 0);     
+    analogWrite(izqB, 0); 
+    analogWrite(derA, 0);  
+    analogWrite(izqA, vel);      
+  }
+  if(estado=='c'){         // Boton Parar
+    analogWrite(derB, 0);     
+    analogWrite(izqB, 0); 
+    analogWrite(derA, 0);    
+    analogWrite(izqA, 0); 
+  }
+  if(estado=='d'){          // Boton DER
+    analogWrite(derB, 0);     
+    analogWrite(izqB, 0);
+    analogWrite(izqA, 0);
+    analogWrite(derA, vel);  
+  } 
+
+  if(estado=='e'){          // Boton Reversa
+    analogWrite(derA, 0);    
+    analogWrite(izqA, 0);
+    analogWrite(derB, vel);  
+    analogWrite(izqB, vel);      
+  }
+  
+}
